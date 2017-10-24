5ac377c5fa2ca310c376056bdc86a9bdae861969
prateekkaushal7886 <prateekkaushal7886@gmail.com>
2017-10-24 21:54:51 +0530

    changes 2

--- resources/views/coordinator.blade.php
+++ resources/views/coordinator.blade.php
@@ -83,41 +83,23 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
       <div class="w3-container w3-card w3-white w3-margin-bottom">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>VIEW DATA</h2>
         <div class="w3-container">
-          <h5 class="w3-opacity"><b><div class="btn-group btn-group-lg"><button type="button" class="btn btn-primary">Information</button></div></b></h5>
+         <div class="btn-group btn-group-lg"><button type="button" class="btn btn-primary">Information</button></div>
           <p>Click the button above to view all the networking data and edit it if needed.</p>
           <hr>
         </div>
+        <br />
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-          <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          <h5 class="w3-opacity"><b>TAGS</b></h5>
+          <div class="btn-group btn-group-lg"><button type="button" class="btn btn-primary">TAGS</button></div>
+          
+          <p>You can create tags,assign tags to alumni and also delete them.Click the button to do it.</p>
           <hr>
         </div>
+        <br />
         <div class="w3-container">
-          <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-        </div>
-      </div>
-
-      <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>W3Schools.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-          <p>Web Development! All I need to know in one place</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>London Business School</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-          <p>Master Degree</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>School of Coding</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-          <p>Bachelor Degree</p><br>
+          <h5 class="w3-opacity"><b>ACCESS</b></h5>
+           <div class="btn-group btn-group-lg"><button type="button" class="btn btn-primary">ACCESS</button></div>
+          <p>You can give access to student members of the data of alumni of our instituion.Click the button to see it. </p><br>
         </div>
       </div>
 
