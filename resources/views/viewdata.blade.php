5b8d3dcae92cd773f2d1f2c0d6804e5e1b58d1e5
MaxAsif <maxasif@gmail.com>
2017-10-26 18:34:19 +0530

    use jquery datatable plugin

--- resources/views/viewdata.blade.php
+++ resources/views/viewdata.blade.php
@@ -1,131 +1,54 @@
-@extends('layouts.app')
+<head>
 
-@section('content')
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-<script>
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css"/>
+
+  <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
+   <script type="text/javascript" src="https://cdn.datatables.net/select/1.2.3/js/dataTables.select.min.js"></script>
+   <script type="text/javascript" src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
+  <script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+      $('#example').DataTable();
+    } );
+  </script>
+</head>
+<body>
+  @include('layouts.navbar')
+  <div class="container">
+
+    <table id="example" class="display" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          <th>ALUMNI</th>
+          <th>EMAIL</th>
+          <th>INDUSTRY</th>
+
+
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach($alumni as $alum)
+        <tr>
+          <td>{{$alum['name']}}</td>        
+          <td>{{$alum['email']}}</td>
+          <td>{{$alum['industry']}}</td>
+
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+
+  </div>
+
+  <script type="text/javascript">
+  // For demo to fit into DataTables site builder...
+  $('#example')
+  .removeClass( 'display' )
+  .addClass('table table-striped table-bordered');
 </script>
-<style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
-</style>
-<body class="w3-light-grey w3-content" style="max-width:1600px">
-
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
-  <div class="w3-container">
-    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
-      <i class="fa fa-remove"></i>
-    </a>
-    <br><br>
-    <h4><b>PORTFOLIO</b></h4>
-    <p class="w3-text-grey">Click on the link below to get data</p>
-  </div>
-  <div class="w3-bar-block">
-    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ALUMNI</a> 
-    <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>COORDINATORS</a> 
-    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>STUDENT_MEMBERS</a>
-  </div>
- <!-- <div class="w3-panel w3-large">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-  </div>
--->
-</nav>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:300px">
-
-  <!-- Header -->
-  <header id="portfolio">
-    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
-    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    <div class="w3-container">
-    <h1><b>My Portfolio</b></h1>
-    <div class="w3-section w3-bottombar w3-padding-16">
-      <span class="w3-margin-right">Filter:</span> 
-      <button class="w3-button w3-black">ALL</button>
-      <button class="w3-button w3-white">TAG1</button>
-      <button class="w3-button w3-white w3-hide-small">TAG2</button>
-      <button class="w3-button w3-white w3-hide-small">TAG3</button>
-    </div>
-    </div>
-  </header>
-  
-  <!-- First Photo Grid-->
- 
-<div class="container">
-  <h2>ALUMNI DETAILS</h2>    
-  <table class="table table-striped" id="example">
-    <thead>
-      <tr>
-        <th>ALUMNI</th>
-        <th>EMAIL</th>
-        <th>INDUSTRY</th>
-        
-
-      </tr>
-    </thead>
-    <tbody>
-      
-      @foreach($alumni as $alum)
-      <tr>
-        <td>{{$alum['name']}}</td>        
-        <td>{{$alum['email']}}</td>
-        <td>{{$alum['industry']}}</td>
-        
-      </tr>
-      @endforeach
-      
-    </tbody>
-  </table>
-</div>
-
-  <!-- Pagination 
-  <div class="w3-center w3-padding-32">
-    <div class="w3-bar">
-      <a href="#" class="w3-bar-item w3-button w3-hover-black">«</a>
-      <a href="#" class="w3-bar-item w3-black w3-button">1</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-black">2</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-black">3</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-black">4</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-black">»</a>
-    </div>
-  </div>
-  -->
-  <!-- Images of Me -->
- 
-  
-  <!-- Contact Section -->
-
-<!-- End page content -->
-</div>
-
-
-
-@endsection
+</body>
+</html>
