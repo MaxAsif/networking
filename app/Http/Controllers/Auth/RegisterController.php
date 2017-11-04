66abe82d1da6651f62011d97658057e79ea77795
MaxAsif <maxasif@gmail.com>
2017-11-05 01:34:13 +0530

    correction in registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,7 +71,7 @@ class RegisterController extends Controller
                 'name' => $data['name'],
                 'email' => $data['email'],
                 'url' => '',
-                'access' => '',
+               
                 ]);
         }
         elseif ($data['type']=='CO')
