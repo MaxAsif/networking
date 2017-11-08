948044c1cd5a84c7e33b306381f655e08753b14f
MaxAsif <maxasif@gmail.com>
2017-11-09 00:07:53 +0530

    changes in acess blade php

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -1,12 +1,14 @@
 <head>
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css"/>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-  <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/select/1.2.3/js/dataTables.select.min.js"></script>
+
+  <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
+  <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
   <script type="text/javascript" charset="utf-8">
     $(document).ready(function() {
@@ -64,8 +66,8 @@
             @foreach($tags as $tag)
             {{$tag['tagname']}}<br>
              <input type="radio" name="{{$tag['id']}}" value="{{$tag['id']}}" id="tagid_{{$tag['id']}}">YES
-             <br><input type="radio" name="{{$tag['id']}}" value="0" id="tagid_{{$tag['id']}}" checked="">NO
-             <br><br>
+             <input type="radio" name="{{$tag['id']}}" value="0" id="tagid_{{$tag['id']}}" checked="">NO
+             <br>
               @endforeach
 
               <input type="submit" value="Submit">
